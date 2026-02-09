7655be423a4f3a92c67e6f6194c3229d4da48f09
AvishMD <avishmd2007@gmail.com>
2026-02-09 11:13:50 +0530

    Add files via upload

---
+++ p4.c
@@ -0,0 +1,13 @@
+Print a Float Value
+	#include <stdio.h>
+
+	int main() {
+   		float a=2.5;
+    		float b=7.25;
+    		printf("%0.1f\n", a);
+    		printf("%0.2f\n", b);
+    
+    
+
+    	return 0;
+}
