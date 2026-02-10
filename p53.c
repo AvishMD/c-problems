b09cf876ecc8e070a3a9c371e3120451d2d277f3
AvishMD <avishmd2007@gmail.com>
2026-02-10 13:24:41 +0530

    Update p53.c

--- p53.c
+++ p53.c
@@ -1,11 +1,14 @@
 
+
 #include <stdio.h>
 
 int main() {
-  int a,b;
-  scanf("%d %d", &a,&b);
-  printf("%d %d ",a/b,a%b);
-  
+    int a;
+    int b;
+    scanf("%d %d", &a,&b);
+    int c=a;
+    printf("%d ", a/=b);
+    printf("%d",c%=b);
 
     return 0;
 }
